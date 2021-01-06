96c7d0dad349511f0a0f22b13522e2bb12b76288
xiang19890319@gmail.com <xiang19890319@gmail.com>
2021-01-06 21:52:56 +0800

    add read me

---
+++ README.md
@@ -0,0 +1,5 @@
+# Ziper
+***
+##purpose
+Ziper is common util for Zip analysing and Zip Modification.
+You can easily use it with command `java -jar ziper.jar -f your.zip`
--- src/test/java/com/ziper/console/ConsoleZipMainTest.java
+++ src/test/java/com/ziper/console/ConsoleZipMainTest.java
@@ -4,6 +4,8 @@ import com.ziper.ZipMain;
 import junit.framework.TestCase;
 import org.junit.Test;
 
+import java.io.IOException;
+
 public class ConsoleZipMainTest extends TestCase {
 
     ZipMain zipMain = new ConsoleZipMain();
@@ -13,7 +15,7 @@ public class ConsoleZipMainTest extends TestCase {
         int exception = 0;
         try {
             zipMain.analysis("not_exists");
-        } catch (IllegalArgumentException e) {
+        } catch (IllegalArgumentException | IOException e) {
             exception++;
         } finally {
             assertTrue(exception>0);
